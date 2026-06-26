4f4f152a19450b48034af73f53956d9c7577b101
Zaio <assuncaozayon@gmail.com>
2026-06-26 17:58:42 -0300

    Implement phone number formatting logic
    Add functionality to format a phone number based on its length.

--- lab 9/exercicio1.py
+++ lab 9/exercicio1.py
@@ -21,3 +21,15 @@ nome = input("Digite seu nome: ")
 
 for i in range(1, len(nome) + 1):
   print(nome[:i])
+
+#exercicio 4
+
+numero = input("Digite o numero: ")
+
+if len(numero) == 8:
+  numero = "9" + numero
+if len (numero) == 9:
+  if numero[0]!= "9":
+    numero = "9" + numero[1:]
+
+print(f"Numero completo: {numero[:5]}-{numero[5:]}")
