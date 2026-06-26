7d07382b270d2f5c176240ba5fc2e9b32829f2e9
Zaio <assuncaozayon@gmail.com>
2026-06-26 17:47:52 -0300

    Add exercises for user input and string manipulation

--- lab 9/exercicio1.py
+++ lab 9/exercicio1.py
@@ -1,6 +1,19 @@
+#exercicio 1
+
 nome = input("Digite seu primeiro nome: ")
 sobrenome = input("Digite seu sobrenome: ")
 
 nomecompleto = nome + " " + sobrenome
 
 print(f"Bem vindo/a, {nomecompleto}")
+
+#exercicio 2
+
+frase = input("Digite a frase: ")
+
+espacos = frase.count(" ")
+
+print("Espaços em branco:", espacos)
+
+#exercicio 3
+
